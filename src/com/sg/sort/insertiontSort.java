2c59b5ecf289fdc2b9cd2e83291031199f12df48
ShGuan <305666742@qq.com>
2019-06-05 14:49:57 +0800

    直接插入排序、希尔排序

--- src/com/sg/sort/insertiontSort.java
+++ src/com/sg/sort/insertiontSort.java
@@ -51,6 +51,29 @@ public class insertiontSort {
         }
     }
 
+    /**
+     * 希尔排序
+     * @param array 待排序数组
+     * @param incremental 增量
+     */
+    public static void shellSort(int[] array, int[] incremental) {
+        int arrayLen = array.length;
+        int increLen = incremental.length;
+        int i, j, temp;
+        for (int k = 0; k < increLen; k++) {
+            // 增量
+            int ink = incremental[k];
+            for (i = ink; i < arrayLen; i++) {
+                // 在子表内进行直接插入排序
+                temp = array[i];
+                for (j = i - ink; j >= 0 && Tools.comPareTo(temp, array[j]) < 0; j -= ink) {
+                    array[j+ink] = array[j];
+                }
+                array[j+ink] = temp;
+            }
+        }
+
+    }
 
 
 
