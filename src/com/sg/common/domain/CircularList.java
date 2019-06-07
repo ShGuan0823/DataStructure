dc9585965d1dc6db9aaa1d0df2cbcece4efc6f45
ShGuan <305666742@qq.com>
2019-06-07 17:09:16 +0800

    循环链表

---
+++ src/com/sg/common/domain/CircularList.java
@@ -0,0 +1,49 @@
+package com.sg.common.domain;
+
+/**
+ * @Description 循环链表
+ * @Author ShGuan
+ * @Date 2019/6/7 16:51
+ **/
+public class CircularList extends LinkList{
+
+    /**
+     * 头结点
+     */
+    private LinkNode head;
+
+    /**
+     * 尾结点
+     */
+    private LinkNode tail;
+
+    public CircularList() {
+        head = new LinkNode();
+        tail = new LinkNode();
+        tail.setNext(head);
+    }
+
+    public CircularList(int n, boolean order) throws Exception {
+        super(n, order);
+        if (order) {
+            createFromTail(n);
+        } else {
+            createFromHead(n);
+        }
+        tail = getTail();
+        tail.setNext(head);
+    }
+
+    public LinkNode getTail() {
+        if (head.getNext() != null) {
+            tail = head.getNext();
+            int n = 0;
+            while (tail.getNext() != null && n < length()) {
+                tail = tail.getNext();
+                ++n;
+            }
+            return tail;
+        }
+        return null;
+    }
+}
